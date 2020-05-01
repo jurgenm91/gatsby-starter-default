3e62095167efe9207be39950de8792f3d56253b1
jurgenm91 <muls.jurgen@gmail.com>
2020-05-01 15:20:08 +0200

    building layout

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
+const Header = ({ siteTitle, siteDescription }) => (
   <header
     style={{
       background: `rebeccapurple`,
@@ -27,6 +27,7 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+      <h2>{siteDescription}</h2>
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,15 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Kaart from "../components/kaart"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div style={{ maxWidth: `200px`, marginBottom: `1.45rem` }}>
       <Image />
+      <Kaart titel="Presentatrice" afbeelding="presentatrice" />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
