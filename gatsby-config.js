4fce6b71d4c43e497fc507081740601a3a69d836
jurgenm91 <muls.jurgen@gmail.com>
2020-05-06 16:06:17 +0200

    Voorpagina tekst bewerkbaar via Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/home`,
+        name: `markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,49 +5,42 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 import MenuKaarten from "../components/menuKaarten"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div className="flex flex-wrap mb-20">
-      <div className="w-1/4 mr-10">
-        <Image />
+import { graphql, useStaticQuery } from "gatsby"
+
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      markdownRemark {
+        html
+      }
+    }
+  `)
+  const content = data.markdownRemark.html
+  window.onload = function html() {
+    document.getElementById("home-content").innerHTML = content
+  }
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div className="flex flex-wrap mb-20">
+        <div className="w-1/4 mr-10">
+          <Image />
+        </div>
+        <div
+          className="bg-shadow bg-yellow-300 h-10 w-full max-w-3xl absolute"
+          style={{
+            zIndex: "-1",
+            filter: "blur(30px)",
+            transform: "rotate(-15deg)",
+            marginTop: "180px",
+            opacity: "0.4",
+          }}
+        ></div>
+        <div id="home-content" className="w-1/2 flex-grow"></div>
       </div>
-      <div
-        className="bg-shadow bg-yellow-300 h-10 w-full max-w-3xl absolute"
-        style={{
-          zIndex: "-1",
-          filter: "blur(30px)",
-          transform: "rotate(-15deg)",
-          marginTop: "180px",
-          opacity: "0.4",
-        }}
-      ></div>
-      <div className="w-1/2 flex-grow">
-        <p>Hallo,</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis commodo
-          ipsum metus, nec commodo magna ultrices vitae. Duis rhoncus ligula ac
-          massa pellentesque tempor. Pellentesque interdum massa mauris, ut
-          vulputate enim gravida quis. Praesent dignissim nunc quis vestibulum
-          iaculis. Praesent ac ex malesuada, cursus nulla ut, feugiat velit.
-        </p>
-        <p>
-          Vestibulum a tortor gravida, placerat sem eget, cursus felis. In
-          placerat nisi a eros vehicula ornare. Cras eleifend, augue et
-          hendrerit ornare, nisi ante ornare nisi, nec congue lorem metus vel
-          velit. Nunc mauris metus, placerat at ipsum eu, efficitur interdum
-          turpis. Cras sed tempus libero, ut tincidunt nunc.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis commodo
-          ipsum metus, nec commodo magna ultrices vitae. Duis rhoncus ligula ac
-          massa pellentesque tempor. Pellentesque interdum massa mauris, ut
-          vulputate enim gravida quis.
-        </p>
-      </div>
-    </div>
-    <MenuKaarten />
-  </Layout>
-)
+      <MenuKaarten />
+    </Layout>
+  )
+}
 
 export default IndexPage
