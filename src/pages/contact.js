0a795f6e1a6d9af9286e8d6f0e54d7f35c896c8b
jurgenm91 <muls.jurgen@gmail.com>
2020-05-15 21:29:29 +0200

    Contactformulier voorzien van data-netlify-honeypot

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -40,6 +40,7 @@ const Contact = () => {
             data-netlify="true"
             data-netlify-honeypot="bot-field"
           >
+            <input type="hidden" name="form-name" value="contact" />
             <input type="hidden" name="bot-field" />
             <p>
               <label>
